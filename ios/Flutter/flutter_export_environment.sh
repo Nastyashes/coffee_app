080d6dd115fe4b6c827bb241a309a5112280abd4
Anastasiia Kravchenko <you@example.com>
2024-09-25 11:29:52 +0300

    new_generate

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\stani\Downloads\flutter_windows_3.19.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\stani\VS Code Project\coffee_app"
+export "FLUTTER_ROOT=D:\Flutter SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\VS Code Project\coffee_app\coffee_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
